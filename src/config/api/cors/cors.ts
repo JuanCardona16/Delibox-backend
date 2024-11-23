b087464d9ec779dcae2da82ad3703526820f11aa
Juan Cardona <jcardonabageth@gmail.com>
2024-11-23 00:08:45 -0500

    fix: update cors origins list

--- src/config/api/cors/cors.ts
+++ src/config/api/cors/cors.ts
@@ -3,7 +3,8 @@ import cors from 'cors'
 const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:5174',
-  'https://delibox-client.onrender.com'
+  'https://delibox-client.onrender.com',
+  'https://delibox-backend.onrender.com'
 ]
 
 export const corsConfig = () => {
